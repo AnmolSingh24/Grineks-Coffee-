6e834c2fbd4af2453e4b7ac3d077a86b6033478f
Anmol Singh <as0796348@gmail.com>
2024-07-18 15:13:09 +0530

    Footer section completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,15 +13,14 @@ const Footer = () => {
                     <div className='flex flex-col text-center p-2 font-semibold text-yellow-800'>
                         <Link to="/about" className='hover:text-yellow-600 pb-2'>ABOUT</Link>
                         <Link to="/menuList" className='hover:text-yellow-600 pb-2'>MENU</Link>
+                        <Link to="/productList" className='hover:text-yellow-600 pb-2'>PRODUCT</Link>
                         <Link to="/facility" className='hover:text-yellow-600 pb-2'>FACILITY</Link>
-                        <Link to="/reviews" className='hover:text-yellow-600 pb-2'>REVIEWS</Link>
                     </div>
                 </div>
                 <div>
-                    <h2 className='text-yellow-800 font-semibold text-3xl px-16 py-6'>Socials</h2>
+                    <p className='text-center text-yellow-800 pt-8'>© 2024 Grind Geeks All rights reserved</p>
                 </div>
             </div>
-            <p className='text-center p-2'>© 2024 Grind Geeks All rights reserved</p>
         </div>
     )
 }
