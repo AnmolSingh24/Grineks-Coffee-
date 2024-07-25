c776abfa8a2a800cd2bf67cef482cfa3df56ffcf
Anmol Singh <as0796348@gmail.com>
2024-07-25 14:08:56 +0530

    product context added

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,5 +1,17 @@
 import React, { createContext, useState } from 'react';
 
+export const MenuContext = createContext();
+
+export const MenuProvider = ({ children }) => {
+    const [menu, setMenu] = useState(null);
+
+    return (
+        <MenuContext.Provider value={{ menu, setMenu }}>
+            {children}
+        </MenuContext.Provider>
+    );
+};
+
 export const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
