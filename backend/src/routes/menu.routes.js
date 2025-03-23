2845961d5dee1139059824332ddc9e356fcc594c
Anmol Singh <as0796348@gmail.com>
2025-03-23 14:34:48 +0530

    changes added

--- backend/src/routes/menu.routes.js
+++ backend/src/routes/menu.routes.js
@@ -4,16 +4,7 @@ import MenuItem  from '../models/menu.model.js';
 import { protect, authorize }  from '../middleware/auth.middleware.js';
 
 // Get all menu items
-router.get('/user-menu', async (req, res) => {
-  try {
-    const menuItems = await MenuItem.find();
-    res.json(menuItems);
-  } catch (err) {
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-router.get('/menu-varieties', async (req, res) => {
+router.get('/all-menu', async (req, res) => {
   try {
     const menuItems = await MenuItem.find();
     res.json(menuItems);
@@ -34,7 +25,7 @@ router.post('/admin-menu', protect, authorize('admin'), async (req, res) => {
 });
 
 // Update menu item (admin only)
-router.put('/:admin-id', protect, authorize('admin'), async (req, res) => {
+router.put('/:id', protect, authorize('admin'), async (req, res) => {
   try {
     const menuItem = await MenuItem.findByIdAndUpdate(
       req.params.id,
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,33 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Facility from './pages/Facility';
-import MenuVarieties from './components/Menu/MenuVarieties';
-import MenuList from './components/Menu/MenuList';
-import ProductList from "./components/Product/ProductList";
-import Cart from './components/CartItem/Cart';
-import './index.css';
-import { CartProvider } from './context/CartContext';
-import WishList from './WishList';
-import TrackOrder from './TrackOrder';
-import ProductSummary from './components/Product/ProductSummary';
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./components/About"
+import MenuList from "./pages/Menu/MenuList"
+import ProductList from "./pages/Product/ProductList"
+import Facility from "./components/Facility"
+import MenuVarieties from "./pages/Menu/MenuVarieties"
+import Cart from "./pages/CartItem/Cart"
+import ProductSummary from "./pages/Product/ProductSummary"
+import AdminPanel from "./pages/Admin/AdminPanel"
 
-const App = () => {
+function App() {
 
   return (
     <>
-      <CartProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/menuList" element={<MenuList />} />
-          <Route path="/productList" element={<ProductList />} />
-          <Route path="/facility" element={<Facility />} />
-          <Route path="/menuVarieties" element={<MenuVarieties />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/ProductList/ProductSummary" element={<ProductSummary />} />
-          <Route path="/wishList" element={<WishList />} />
-          <Route path="/trackOrder" element={<TrackOrder />} />
-        </Routes>
-      </CartProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menuList" element={<MenuList />} />
+        <Route path="/productList" element={<ProductList />} />
+        <Route path="/facility" element={<Facility />} />
+        <Route path="/menuVarieties" element={<MenuVarieties />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/ProductList/ProductSummary" element={<ProductSummary />} />
+        {/* <Route path="/wishList" element={<WishList />} />
+        <Route path="/trackOrder" element={<TrackOrder />} /> */}
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
